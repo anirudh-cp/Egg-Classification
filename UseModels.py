673cb27b45d4217460668d24afa850f5d5476612
Anirudh <cp.anirudh@outlook.com>
2022-05-30 23:38:07 +0530

    Model Tester
    The model tester has now been given callbacks for each button.
    It is ready to accept a model, an image and classify it.

--- UseModels.py
+++ UseModels.py
@@ -1,9 +1,13 @@
-from main import CNN, RESIZED
+from main import CNN, RESIZED, classes
 import torch
 from PIL import Image
 import torchvision.transforms as transforms
 
 import tkinter as tk
+from tkinter import filedialog as fd
+from tkinter import messagebox as mb
+from PIL import Image,ImageTk
+from pathlib import Path
 
 
 class App(tk.Tk):
@@ -16,11 +20,17 @@ class App(tk.Tk):
         Modelframe.pack(padx=10, pady=10, fill=tk.X)
         Modelframe.pack()
         
-        self.selectModelButton = tk.Button(Modelframe, text='Select Model', command=self.handleModelPath)
-        self.selectModelButton.pack(padx=5, pady=7, side=tk.LEFT, ipadx=3)
-        
         self.ModelPath = tk.StringVar()
         self.ModelPath.set(' None Selected')
+        self.ModelPathText = ''
+        
+        self.selectModelButton = tk.Button(Modelframe, text='Select Model', 
+                                           command=lambda : 
+                                               self.handleModelLoad('Open Model',
+                                                                    [('PyTorch Machine Learning Model (*.pth)', '*.pth')],
+                                                                    self.ModelPath))
+        self.selectModelButton.pack(padx=5, pady=7, side=tk.LEFT, ipadx=3)
+        
         self.selectModelLabel = tk.Label(Modelframe, textvariable=self.ModelPath, borderwidth=2, relief=tk.GROOVE, anchor=tk.W)
         self.selectModelLabel.pack(padx=5, pady=7, ipady=3, fill=tk.X)
         
@@ -32,18 +42,23 @@ class App(tk.Tk):
         ImagePathFrame = tk.Frame(Imageframe)
         ImagePathFrame.pack(fill=tk.X)
         
-        self.selectImageButton = tk.Button(ImagePathFrame, text='Select Image', command=self.handleModelPath)
-        self.selectImageButton.pack(padx=5, pady=7, side=tk.LEFT, ipadx=3)
-        
         self.ImagePath = tk.StringVar()
         self.ImagePath.set(' None Selected')
+        self.ImagePathText = ''
+        
+        self.selectImageButton = tk.Button(ImagePathFrame, text='Select Image', 
+                                           command=lambda : 
+                                               self.handleImageLoad('Open Image', 
+                                                                    [('PNG (*.png)', '*.png')],
+                                                                    self.ImagePath))
+        self.selectImageButton.pack(padx=5, pady=7, side=tk.LEFT, ipadx=3)
+
         self.selectImageLabel = tk.Label(ImagePathFrame, textvariable=self.ImagePath, borderwidth=2, relief=tk.GROOVE, anchor=tk.W)
         self.selectImageLabel.pack(padx=5, pady=7, ipady=3, fill=tk.X)
         
-        self.ImageCanvas = tk.Canvas(Imageframe, background="white", borderwidth=2, relief=tk.GROOVE,
-                                     width=RESIZED[1], height=RESIZED[0])
+        self.ImageCanvas = tk.Canvas(Imageframe, borderwidth=2, relief=tk.GROOVE,
+                                     width=RESIZED[1], height=RESIZED[0])    
         self.ImageCanvas.pack(padx=10, pady=7)
-
 
         Outframe = tk.LabelFrame(self, text='Output', relief=tk.GROOVE, bd=4)
         Outframe.pack(padx=10, pady=10, fill=tk.X)
@@ -55,27 +70,57 @@ class App(tk.Tk):
         self.OutLabel = tk.Label(Outframe, textvariable=self.OutClass, borderwidth=2, relief=tk.GROOVE, anchor=tk.W)
         self.OutLabel.pack(padx=5, pady=7, ipady=3, fill=tk.X)
         
-        self.classifyButton = tk.Button(Outframe, text='Classify')
+        self.classifyButton = tk.Button(Outframe, text='Classify', command=self.handleClassification)
         self.classifyButton.pack(padx=10, pady=(10, 7), fill=tk.X)
 
-        self.resizable(0, 0)
+        self.resizable(1, 0)
 
 
-    def handleModelPath(self):
-        self.ModelPath.set(' ABC')
-
-
-def getImage():
-    # Read a PIL image
-    image = Image.open('PATH')
+    def __browse(self, title, fileTypes, Stringvar):
+        path = fd.askopenfilename(parent=self, title=title, filetypes=fileTypes)
+        path_ = Path(path)
+        fileName = path_.name
+        if fileName == '':
+            fileName = 'None Selected'
+        Stringvar.set(' ' + fileName)
+        return path
     
-    # Define a transform to convert PIL 
-    # image to a Torch tensor
-    transform = transforms.Compose([transforms.Resize(RESIZED), transforms.ToTensor()])
     
-    # transform = transforms.PILToTensor()
-    # Convert the PIL image to Torch tensor
-    img_tensor = transform(image)
+    def handleModelLoad(self, title, fileTypes, Stringvar):
+        self.ModelPathText = self.__browse(title, fileTypes, Stringvar)
+        self.OutClass.set(' Class: Undetermined')
+        
+        
+    def handleImageLoad(self, title, fileTypes, Stringvar):
+        self.ImagePathText = self.__browse(title, fileTypes, Stringvar)
+        self.OutClass.set(' Class: Undetermined')
+        if self.ImagePathText != '':
+            self.image = ImageTk.PhotoImage(Image.open(self.ImagePathText).resize((RESIZED[1], RESIZED[0])))     
+            self.ImageCanvas.create_image(3, 3, anchor=tk.NW, image=self.image)    
+            self.ImageCanvas.image = self.image  
+
+
+    def handleClassification(self):
+        # Read a PIL image
+        
+        if self.ImagePathText == '' or self.ModelPathText == '':
+            mb.showerror(title='Input Error', message='Please specify both model and image file.')
+            return
+            
+        image = Image.open(self.ImagePathText)
+        transform = transforms.Compose([transforms.Resize(RESIZED), transforms.ToTensor()])
+        img_tensor = transform(image)
+        
+        model = CNN()
+        model.load_state_dict(torch.load(self.ModelPathText))
+    
+        out = model(img_tensor)
+        # Pick index with highest probability
+        _, preds  = torch.max(out, dim=1)
+        # Retrieve the class label
+        predLabel = classes[preds[0].item()]
+        
+        self.OutClass.set(f' Class: {predLabel}')
 
 
 def main():
